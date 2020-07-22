26ee3ece89d03618b848883e49a780a3bea5ca5b
Haley Collard <collard.haley20@gmail.com>
2020-07-21 21:11:10 -0400

    crossword solver complete

--- xword.py
+++ xword.py
@@ -2,20 +2,39 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Haley Collard"
 
-# YOUR HELPER FUNCTION GOES HERE
+import re
+
+
+def make_dict():
+    word_dict = {}
+    with open("dictionary.txt", 'r') as f:
+        words = f.read().split()
+        for word in words:
+            l = len(word)
+            word_dict.setdefault(l, [])
+            word_dict[l].append(word)
+    return word_dict
 
 
 def main():
-    with open('dictionary.txt') as f:
-        words = f.read().split()
+    word_dict = make_dict()
 
-    test_word = raw_input(
+    test_word = input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    re_word = test_word.replace(' ', '\S')
+    # re_search_word = f'{re_word}'
+    word_length = len(test_word)
+    possible_word_matches = []
+    for word in word_dict[word_length]:
+        match = re.search(re_word, word)
+        if match:
+            possible_word_matches.append(match.string)
 
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    print(possible_word_matches)
+
+    # raise NotImplementedError('Please complete this')
 
 
 if __name__ == '__main__':
